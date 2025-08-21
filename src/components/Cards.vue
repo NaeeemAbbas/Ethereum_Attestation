3f4a6e07dba18a316bdc7223977d3e2eaaee70ce
NaeeemAbbas <naeemqurashii674@gmail.com>
2025-08-21 05:17:21 -0700

    updates

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, computed } from 'vue'; // Import computed
-import img1 from '../assets/img1.png'
+
 import img2 from '../assets/img2.jpg'
 import img3 from '../assets/img3.jpg'
 import img4 from '../assets/img4.jpg'
